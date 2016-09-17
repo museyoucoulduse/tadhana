ba79f372145e3191ce0650aef600d5cccb5464cf
Szymon Blaszczynski <museyoucoulduse@icloud.com>
2016-09-17 09:05:39 -0700

    Gulp serve

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var inject = require('gulp-inject');
+var nodemon = require('gulp-nodemon');
 var jsFiles = ['*.js', 'backend/**/*.js'];
 gulp.task('style', function () {
     gulp.src(jsFiles).pipe(jshint()).pipe(jshint.reporter('jshint-stylish', {
@@ -12,6 +14,28 @@ gulp.task('inject', function () {
     var options = {
         bowerJson: require('./bower.json')
         , directory: './public/lib'
-    }
-    return gulp.src('./public/index.html').pipe(wiredep(options)).pipe(gulp.dest('./public/'))
-})
+    };
+    var injectSrc = gulp.src(['./public/css/**/*.css', './public/js/**/*.js'], {
+        read: true
+    });
+    var injectOptions = {
+        ignorePath: 'public'
+    };
+    return gulp.src('./public/index.html').pipe(wiredep(options)).pipe(inject(injectSrc, injectOptions)).pipe(gulp.dest('./public/'));
+});
+gulp.task('serve', ['style', 'inject'], function () {
+    var options = {
+        script: 'backend/index.js'
+        , delayTime: 2
+        , env: {
+            'PORT': 3000
+            , 'IP': 'localhost'
+        }
+        , watch: jsFiles
+    };
+    count = 0;
+    return nodemon(options).on('restart', function () {
+
+        console.log('Restart:', ++count);
+    });
+});
