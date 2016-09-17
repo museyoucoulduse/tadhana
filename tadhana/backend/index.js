438a71fa5157f925da0c66f5cded447969b85d4c
Fun Looking Guy <museyoucoulduse@icloud.com>
2016-09-17 14:55:41 +0000

    Add my package sexjs.

--- tadhana/backend/index.js
+++ tadhana/backend/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var morgan = require("morgan");
 var storyRouter = require('./router.js');
+var sex = require("sexjs");
 
 var app = express();
 app.use(morgan('dev'));
@@ -8,5 +9,5 @@ app.use(express.static(__dirname + '/../public/'));
 app.use('/story', storyRouter);
 
 app.listen(process.env.PORT, process.env.IP, function () {
-    console.log(`Server is running at http://${process.env.IP}:${process.env.PORT}`);
+    console.log(`Server ${sex.neuter} is running at http://${process.env.IP}:${process.env.PORT}`);
 });
